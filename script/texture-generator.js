8342f8512c5a8381b8f25eb9f1ab8fb9fc81379e
Tomasz Kociołek <tomasz.kociolek@k2.pl>
2015-02-28 23:16:25 +0100

    perlin noise progress

--- script/texture-generator.js
+++ script/texture-generator.js
@@ -1,4 +1,4 @@
-/* globals ClassicalNoise */
+/* globals Perlin */
 
 Object.prototype.extend = function(obj) {
     for(var i in obj)
@@ -9,14 +9,14 @@ Object.prototype.extend = function(obj) {
 function TextureGenerator(options) {
 
 	var defaults = {
-		width: 100,
-		height: 100,
+		width: 300,
+		height: 300,
 		gradientGridSize: 64
 	}
 
 	this.settings = defaults.extend(options);
 	this.context = this.createContext(this.settings.width, this.settings.height);
-	this.noiseGenerator = new ClassicalNoise();
+	this.noiseGenerator = new PerlinOriginal();
 	this.generate();
 }
 
@@ -28,71 +28,19 @@ TextureGenerator.prototype.createContext = function(width, height) {
 };
 
 TextureGenerator.prototype.generate = function() {
-	var value = 0;
+	var noise = this.noiseGenerator.noise,
+        value = 0;
 	for (var x = 0; x < this.settings.width; x++) {
 		for (var y = 0; y < this.settings.height; y++) {
-			value = Math.floor(Math.abs(this.noiseGenerator.noise(x/this.settings.width, y/this.settings.height, 0)) * 255);
+            var nx = x/this.settings.width;
+            var ny = y/this.settings.height;
+            var n1 = this.noiseGenerator.noise(nx, ny);
+            var n2 = this.noiseGenerator.noise(nx*2, ny*2)/2;
+            var n3 = this.noiseGenerator.noise(nx*4, ny*4)/4;
+
+			value = Math.abs(((n1 + n2 + n3 + n1 + n2 + n3) * 255)) & 0x000000ff;
 			this.context.fillStyle = 'rgba('+value+', '+value+', '+value+', 1)';//'rgb('+value+','+value+','+value+')';
 			this.context.fillRect( x, y, 1, 1 );
 		}
 	}
 }
-
-function Perlin(options) {
-    var defaults = {
-        persistence: 1/2,
-        octaves: 6
-    }
-
-    this.settings = defaults.extend(options);
-
-
-}
-
-Perlin.prototype.preprocess = function(first_argument) {
-    this.frequecies = [];
-    this.aplitudes = [];
-
-    for (var i = 0; i < octaves; i++) {
-        this.frequecies.push(Math.pow(2, i));
-        this.aplitudes.push(Math.pow(this.settings.persistence, i));
-    }
-};
-
-function MersenneTwister(seed) {
-	this.MT = new Array(623);
-	this.index = 0;
-	this.MT[0] = seed;
-
-	for (var i = 1, mtLen = this.MT.length; i < mtLen; i++) {
-		this.MT[i] = (1812433253 * (this.MT[i-1] ^ (this.MT[i-1] >> 30)) + i) & 0xFFFFFFFF;
-	}
-
-	this.generateNumbers();
-}
-
-MersenneTwister.prototype.random = function() {
-    var y = this.MT[this.index];
-    y = y ^ (y >> 11);
-    y = y ^ ((y << 7) & (2636928640));
-    y = y ^ ((y << 15) & (4022730752));
-    y = y ^ (y >> 18);
-
-    this.index = (this.index + 1) % 624;
-
-    return y
-};
-
-MersenneTwister.prototype.generateNumbers = function() {
-    for (var i = 0; i < 623; i++) {
-        var y = (this.MT[i] & 0x80000000) + (this.MT[(i+1) % 624] & 0x7fffffff);
-        this.MT[i] = this.MT[(i + 397) % 624] ^ (y >> 1);
-        if ((y % 2) !== 0) {
-            this.MT[i] = this.MT[i] ^ (2567483615);
-        }
-    }
-}
-
-mt = new MersenneTwister(0);
-console.log(mt.random());
-console.log(mt.random());
